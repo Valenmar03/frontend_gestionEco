a799e98906ae464043444e9bf9afaf6ac171c85e
Valenmar03 <valenmrtnz8@gmail.com>
2025-03-28 12:53:23 -0300

    Actulizo la seleccion de productos para poder aumentar la cantidad en mas de 1 por vez

--- src/components/Sales/SalesForm.tsx
+++ src/components/Sales/SalesForm.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { TrashIcon } from "@heroicons/react/20/solid";
 import { Product } from "../../types";
 import { getProducts } from "../../api/productAPI";
 import SalesFormFields from "./SalesFormFields";
@@ -11,6 +11,7 @@ export default function SalesForm() {
       quantity: number;
    };
    const [prodArray, setProdArray] = useState<prodArrayType[]>([]);
+   const [ error, setError ] = useState("")
 
    const { data: products } = useQuery({
       queryKey: ["products"],
@@ -26,29 +27,25 @@ export default function SalesForm() {
 
       setProdArray([
          ...prodArray,
-         { product: products?.find((prod) => prod._id === id)!, quantity: 1 },
+         { product: products?.find((prod) => prod._id === id)!, quantity: 0 },
       ]);
    };
 
-   const handleAddQty = (id: Product["_id"]) => {
+   const handleChangeQty = (id: Product["_id"], qty: number) => {
+      if(qty < 0){
+         setError("La cantidad debe ser mayor a 0 (cero)")
+         return 
+      }
       const updatedProdArray = prodArray.map((prod) => {
-         prod.product._id === id && prod.quantity++;
-         return prod;
+         return prod.product._id === id 
+            ? { ... prod, quantity: qty}
+            : prod
       });
       setProdArray(updatedProdArray);
    };
 
-   const handleSubtractQty = (id: Product["_id"]) => {
-      const product = prodArray.find((prod) => prod.product._id === id);
-      if (product?.quantity === 1) {
-         setProdArray(prodArray.filter((prod) => prod.product._id !== id));
-      } else {
-         const updatedProdArray = prodArray.map((prod) => {
-            prod.product._id === id && prod.quantity--;
-            return prod;
-         });
-         setProdArray(updatedProdArray);
-      }
+   const handleDelete = (id: Product["_id"]) => {
+      setProdArray(prodArray.filter((prod) => prod.product._id !== id));
    };
 
    return (
@@ -62,8 +59,9 @@ export default function SalesForm() {
 
             <div className=" flex flex-col col-span-3 space-y-2">
                <div className="flex justify-between">
-                  <label htmlFor="products" className="text-xl">
+                  <label htmlFor="products" className="text-xl flex gap-3 items-center">
                      Productos
+                     {error && <span className="text-base text-red-500">{error}</span>}
                   </label>
                </div>
 
@@ -77,29 +75,30 @@ export default function SalesForm() {
                         const { product, quantity } = prod;
                         return (
                            <div
-                              className="flex justify-between first-of-type:border-t-0 border-t-2 border-gray-300 py-2 duration-200 p-3"
+                              className="grid grid-cols-6 items-center first-of-type:border-t-0 border-t-2 border-gray-300 py-2 duration-200 p-3"
                               key={product._id}
                            >
-                              <p className="text-center">
+                              <p className="text-center col-span-4">
                                  {`${product.type} x  ${product.weight}${
                                     product.haveWeight ? "Kg." : "mL."
                                  } `}
                               </p>
-                              <p className="flex items-center gap-2">
-                                 <PlusIcon
-                                    className="size-5 cursor-pointer"
-                                    onClick={() =>
-                                       handleAddQty(product._id)
-                                    }
-                                 />
-                                 {quantity}
-                                 <MinusIcon
-                                    className="size-5 cursor-pointer"
-                                    onClick={() =>
-                                       handleSubtractQty(product._id)
-                                    }
-                                 />
-                              </p>
+                              <input
+                                 type="number"
+                                 value={quantity}
+                                 onChange={(e) =>
+                                    handleChangeQty(
+                                       prod.product._id,
+                                       Number(e.target.value)
+                                    )
+                                 }
+                                 className="col-span-1 text-center bg-white rounded-md"
+                              />
+
+                              <TrashIcon
+                                 className="size-5 cursor-pointer col-span-1 mx-auto text-red-500 hover:scale-110 duration-150"
+                                 onClick={() => handleDelete(product._id)}
+                              />
                            </div>
                         );
                      })
