a8bcb2bf1b0da16ddc4ac674aa2d8ce473b41c37
Valenmar03 <valenmrtnz8@gmail.com>
2025-03-31 17:25:12 -0300

    Obtengo lo datos del formulario

--- src/components/Sales/SalesForm.tsx
+++ src/components/Sales/SalesForm.tsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import SalesFormFields from "./SalesFormFields";
 import SalesFormProds from "./SalesFormProds";
+import { Client } from "../../types";
 
 export default function SalesForm() {
+   const [ cliente, setCliente ] = useState<Client>({
+      _id: "",
+      name: "",
+      phoneNumber: "",
+      address: "",
+      cuil: "",
+   });
+   const [ iva, setIva ] = useState(false)
+   const [ dto, setDto ] = useState(0)
+   const [ type, setType ] = useState("")
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      console.log(cliente, iva, dto, type)
+   };
+
    return (
       <>
-         <form className="grid grid-cols-6  bg-white px-20 pt-5 pb-10 rounded-lg shadow-md gap-10 mt-10 w-4/5 mx-auto">
+         <form
+            onSubmit={handleSubmit}
+            className="grid grid-cols-6  bg-white px-20 pt-5 pb-10 rounded-lg shadow-md gap-10 mt-10 w-4/5 mx-auto"
+         >
             <h2 className="text-5xl col-span-6 mx-auto font-bold text-royal-purple-500">
                Agregar Venta
             </h2>
-
-            <SalesFormFields />
-
+            <SalesFormFields setCliente={setCliente} setIva={setIva} setDto={setDto} setType={setType} />
             <SalesFormProds />
+            <input
+               type="submit"
+               value="Crear Venta"
+               className="col-span-6 text-3xl bg-royal-purple-600 text-white font-semibold w-4/5 mx-auto p-2 rounded-md hover:bg-royal-purple-500 cursor-pointer duration-200"
+            />
          </form>
       </>
    );
--- src/components/Sales/SalesFormFields.tsx
+++ src/components/Sales/SalesFormFields.tsx
@@ -2,98 +2,122 @@ import { PlusIcon } from "@heroicons/react/20/solid";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { getClients } from "../../api/clientAPI";
+import { Client } from "../../types";
 
-export default function () {
-    const { data: clients } = useQuery({
-          queryKey: ["clients"],
-          queryFn: getClients,
-       });
-  return (
-    <>
-      <div className="flex flex-col space-y-2 col-span-3">
-               <div className="flex justify-between">
-                  <label htmlFor="client" className="text-xl">
-                     Cliente
-                  </label>
-                  <Link
-                     className="p-1 bg-royal-purple-600 text-white rounded-full cursor-pointer hover:bg-royal-purple-500 duration-200"
-                     to="/clients"
-                  >
-                     <PlusIcon className="size-6" />
-                  </Link>
-               </div>
-               <select
-                  name="client"
-                  id="client"
-                  className="bg-gray-200/50 p-2 rounded-lg text-lg"
+type SalesFormFieldsProps = {
+   setCliente: React.Dispatch<React.SetStateAction<Client>>;
+   setIva: React.Dispatch<React.SetStateAction<boolean>>;
+   setDto: React.Dispatch<React.SetStateAction<number>>;
+   setType: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function SalesFormFields({
+   setCliente,
+   setIva,
+   setDto,
+   setType,
+}: SalesFormFieldsProps) {
+   const { data: clients } = useQuery({
+      queryKey: ["clients"],
+      queryFn: getClients,
+   });
+
+   const handleChangeClient = (id: string) => {
+      if (clients) {
+         setCliente(clients.find((client) => client._id === id)!);
+      }
+   };
+
+   return (
+      <>
+         <div className="flex flex-col space-y-2 col-span-3">
+            <div className="flex justify-between">
+               <label htmlFor="client" className="text-xl">
+                  Cliente
+               </label>
+               <Link
+                  className="p-1 bg-royal-purple-600 text-white rounded-full cursor-pointer hover:bg-royal-purple-500 duration-200"
+                  to="/clients"
                >
-                  <option value="0" className="text-gray-500 bg-white">
-                     --Seleccione un cliente--
-                  </option>
-                  <option value="0" className=" bg-white">
-                     Mercado Libre
-                  </option>
-                  {clients?.map((client) => (
-                     <option
-                        key={client._id}
-                        value={client._id}
-                        className="bg-white"
-                     >
-                        {client.name}
-                     </option>
-                  ))}
-               </select>
+                  <PlusIcon className="size-6" />
+               </Link>
             </div>
-            <div className="flex gap-4 col-span-2">
-               <div className="flex flex-col space-y-2 w-1/2">
-                  <label htmlFor="iva" className="text-xl">
-                     Iva
-                  </label>
-                  <select
-                     name="iva"
-                     id="iva"
-                     className="bg-gray-200/50 p-2 rounded-lg text-lg"
+            <select
+               name="client"
+               id="client"
+               className="bg-gray-200/50 p-2 rounded-lg text-lg"
+               onChange={(e) => handleChangeClient(e.target.value)}
+            >
+               <option value="0" className="text-gray-500 bg-white">
+                  --Seleccione un cliente--
+               </option>
+               <option value="0" className=" bg-white">
+                  Mercado Libre
+               </option>
+               {clients?.map((client) => (
+                  <option
+                     key={client._id}
+                     value={client._id}
+                     className="bg-white"
                   >
-                     <option value="1" className="bg-white">
-                        Sí
-                     </option>
-                     <option value="0" className="bg-white">
-                        No
-                     </option>
-                  </select>
-               </div>
-               <div className="flex flex-col space-y-2 w-1/2">
-                  <label htmlFor="dto" className="text-xl">
-                     Descuento
-                  </label>
-                  <input
-                     type="number"
-                     id="dto"
-                     className="bg-gray-200/50 p-2 rounded-lg text-lg"
-                     placeholder="% de Dto."
-                  />
-               </div>
-            </div>
-            <div className="flex flex-col space-y-2 col-span-1">
-               <label htmlFor="type" className="text-xl">
-                  Tipo de Venta
+                     {client.name}
+                  </option>
+               ))}
+            </select>
+         </div>
+         <div className="flex gap-4 col-span-2">
+            <div className="flex flex-col space-y-2 w-1/2">
+               <label htmlFor="iva" className="text-xl">
+                  Iva
                </label>
                <select
-                  name="type"
-                  id="type"
+                  name="iva"
+                  id="iva"
                   className="bg-gray-200/50 p-2 rounded-lg text-lg"
+                  onChange={(e) => setIva(e.target.value == "true")}
                >
-                  <option value="wholesalePrice" className="bg-white">
-                     Mayorista
+                  <option value="true" className="bg-white">
+                     Sí
                   </option>
-                  <option value="retailPrice" className="bg-white">
-                     Minorista
-                  </option>
-                  <option value="MercadoLibrePrice" className="bg-white">
-                     Mercado Libre
+                  <option value="false" className="bg-white">
+                     No
                   </option>
                </select>
             </div>
-    </>
-  )
+            <div className="flex flex-col space-y-2 w-1/2">
+               <label htmlFor="dto" className="text-xl">
+                  Descuento
+               </label>
+               <input
+                  type="number"
+                  id="dto"
+                  className="bg-gray-200/50 p-2 rounded-lg text-lg"
+                  placeholder="% de Dto."
+                  onChange={e => setDto(+e.target.value)}
+               />
+            </div>
+         </div>
+         <div className="flex flex-col space-y-2 col-span-1">
+            <label htmlFor="type" className="text-xl">
+               Tipo de Venta
+            </label>
+            <select
+               name="type"
+               id="type"
+               className="bg-gray-200/50 p-2 rounded-lg text-lg"
+               onChange={(e) => setType(e.target.value)}
+            >
+               <option value="wholesalePrice" className="bg-white">
+                  Mayorista
+               </option>
+               <option value="retailPrice" className="bg-white">
+                  Minorista
+               </option>
+               <option value="MercadoLibrePrice" className="bg-white">
+                  Mercado Libre
+               </option>
+            </select>
+         </div>
+      </>
+   );
 }
