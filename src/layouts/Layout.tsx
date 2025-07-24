78e5c5da3b3552e97bb0432ba4c06a07d8c6b0b0
Valenmar03 <valenmrtnz8@gmail.com>
2025-07-24 12:31:54 -0300

    Mejoro el layout haciendolo responsive

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,13 +3,27 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HeaderLinks from "../components/HeaderLinks";
 import { pages } from "../data";
-import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/20/solid";
+import {
+   ArrowRightStartOnRectangleIcon,
+   Bars3Icon,
+} from "@heroicons/react/20/solid";
 import { useAuth } from "../hooks/useAuth";
 import Spinner from "../components/Spinner";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
    const navigate = useNavigate();
    const { isError, isLoading } = useAuth();
+
+   const [showNav, setShowNav] = useState(true);
+   const [isMobile, setIsMobile] = useState(false);
+
+   useEffect(() => {
+      if (window.innerWidth < 640) {
+         setIsMobile(true)
+         setShowNav(false);
+      }
+   }, []);
 
    const logOut = () => {
       localStorage.removeItem("AUTH_TOKEN");
@@ -38,27 +52,44 @@ export default function Layout() {
 
    return (
       <>
-         {/* Header */}
-         <header className="bg-vida-loca-200 p-4 shadow">
-            <div className="max-w-screen-xl mx-auto flex justify-between items-center">
-               <img
-                  src="/LogoTexto.png"
-                  alt="Logo Ecorgánico"
-                  className="w-64 sm:w-72"
-               />
+         <header className="bg-vida-loca-200 p-4 shadow fixed w-full top-0 z-50">
+            <div className="max-w-screen mx-auto flex justify-between items-center px-4 sm:px-10">
+               <div className="flex items-center gap-4 sm:gap-10">
+                  <Bars3Icon
+                     className="size-10 text-vida-loca-800 cursor-pointer hover:bg-vida-loca-300 p-1 rounded-md"
+                     onClick={() => setShowNav(!showNav)}
+                  />
+                  <img
+                     src={`${ isMobile ? "/Logo.png" : "/LogoTexto.png"}`}
+                     alt="Logo Ecorgánico"
+                     className="w-12 sm:w-52"
+                  />
+               </div>
                <ArrowRightStartOnRectangleIcon
-                  className="w-10 h-10 text-red-500 cursor-pointer"
+                  className="w-9 h-9 text-red-500 cursor-pointer"
                   onClick={logOut}
                   title="Cerrar sesión"
                />
             </div>
          </header>
 
-         {/* Layout principal */}
-         <div className="flex min-h-screen bg-gray-100">
-            {/* Sidebar */}
-            <aside className="w-64 bg-vida-loca-700/90 text-white">
-               <nav className="flex flex-col">
+         {showNav && (
+            <div
+               className="fixed inset-0 z-30 bg-black opacity-50 sm:hidden"
+               onClick={() => setShowNav(false)}
+            />
+         )}
+
+         <div className="flex pt-20 bg-gray-100 relative">
+            <aside
+               className={`
+                  fixed sm:static z-40 top-0 left-0 min-h-screen w-64 bg-vida-loca-700 sm:bg-vida-loca-700/90 text-white
+                  transform transition-transform duration-300 ease-in-out
+                  ${showNav ? "translate-x-0" : "-translate-x-full"}
+                  sm:translate-x-0 sm:block
+               `}
+            >
+               <nav className="flex flex-col mt-20 sm:mt-0">
                   <HeaderLinks page={homePage} />
                   {pages.map((page) => (
                      <HeaderLinks key={page.title} page={page} />
@@ -66,19 +97,17 @@ export default function Layout() {
                </nav>
             </aside>
 
-            {/* Contenido principal */}
-            <div className="flex-1 flex flex-col px-6 py-8">
-               <main className="flex-grow bg-white p-6 rounded shadow">
+            <div className="flex-1 flex flex-col px-6 py-8 sm:px-50">
+               <main className="bg-white p-6 rounded shadow pb-10">
                   <Outlet />
                </main>
 
                <footer className="mt-10 border-t pt-4 text-center text-gray-600 text-sm">
-                  © 2024 Valentín Martinez – Todos los derechos reservados
+                  © 2024 Valentín Martinez 
                </footer>
             </div>
          </div>
 
-         {/* Toasts */}
          <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
       </>
    );
