c7d47effe3594883aae842f529c2bda64b438e2d
Valenmar03 <valenmrtnz8@gmail.com>
2025-08-26 16:34:40 -0300

    Arreglo modal para vistas mobile

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -28,14 +28,14 @@ export default function ModalComponent({
    if (!isOpen) return null;
 
    return (
-      <div
-         className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+      <div  
+         className="fixed inset-0 z-50 flex lg:items-center justify-center bg-black/50"
          onClick={() => setIsOpen(false)}
          aria-modal="true"
          role="dialog"
       >
          <div
-            className="bg-white p-6 rounded-lg shadow-xl max-w-3xl w-full relative animate-fade-in transform scale-100"
+            className="bg-white max-w-screen mt-20 p-6 rounded-lg shadow-xl md:max-w-3xl w-full relative h-fit animate-fade-in transform scale-100"
             onClick={(e) => e.stopPropagation()}
          >
             {children}
