ca81f1ecb673d59f868290b81d093e6183916635
Valenmar03 <valenmrtnz8@gmail.com>
2025-09-17 18:25:57 -0300

    Mejoro las cards de ventas

--- src/components/Sales/SalesCard.tsx
+++ src/components/Sales/SalesCard.tsx
@@ -7,34 +7,42 @@ export default function SalesCard({ sale }: { sale: Sale }) {
       <>
          <Link
             to={`/sales/${sale._id}`}
-            className="flex flex-col justify-between p-3 bg-gray-50 shadow-md rounded-lg col-span-1 hover:scale-105 duration-300 cursor-pointer"
+            className="flex flex-col justify-between bg-royal-purple-100 shadow-lg rounded-2xl p-5 hover:shadow-xl hover:-translate-y-1 duration-300 cursor-pointer"
          >
-            <div>
-               <p className="text-sm text-right text-royal-purple-700">
-                  {formatDate(sale.createdAt)}
-               </p>
-               <h2 className="text-2xl font-bold text-royal-purple-600 gap-3 flex items-end">
-                  {sale.client.name}
-                  <span className="text-base opacity-70">
+            <div className="flex items-center justify-between mb-1">
+               <div>
+                  <h2 className="text-2xl font-semibold text-royal-purple-700">
+                     {sale.client.name}
+                  </h2>
+                  <span className="text-sm text-royal-purple-500">
                      {translateType(sale.type)}
                   </span>
-               </h2>
+               </div>
+               <p className="text-sm text-gray-500">
+                  {formatDate(sale.createdAt)}
+               </p>
+            </div>
+
+            <div className="divide-y divide-gray-200">
                {sale.products.map((prod) => (
                   <div
                      key={prod.product}
-                     className="flex justify-between items-center mt-2 first-of-type:border-t-0 border-t-2 border-gray-300"
+                     className="flex justify-between py-2 text-gray-700"
                   >
-                     <p className="text">{prod.product}</p>
-                     <p>{prod.quantity}</p>
+                     <p className="truncate">{prod.product}</p>
+                     <p className="font-medium">{prod.quantity}</p>
                   </div>
                ))}
             </div>
-            <div>
-               <p className="text-2xl font-bold text-royal-purple-600 mt-4">
-                  Total Neto: {formatCurrency(sale.total.net)}
+
+            <div className="mt-2 border-t border-gray-200 pt-4">
+               <p className="text-sm text-gray-500">Total Neto</p>
+               <p className="text-lg font-semibold text-royal-purple-600">
+                  {formatCurrency(sale.total.net)}
                </p>
-               <p className="text-2xl font-bold text-royal-purple-600">
-                  Total Bruto: {formatCurrency(sale.total.gross)}
+               <p className="text-sm font-bold text-gray-500 mt-2">Total Bruto</p>
+               <p className="text-2xl font-bold text-royal-purple-700">
+                  {formatCurrency(sale.total.gross)}
                </p>
             </div>
          </Link>
